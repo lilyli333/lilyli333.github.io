147a950ccd2140ef9eb3f36654a573d63b0c24e6
Lily Li <lily.jiayu@gmail.com>
2020-10-16 08:46:20 -0500

    build folder

---
+++ build/precache-manifest.0f4f94d119762f850ff847f2712659bd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1287adf4f74f32f24fc34d5131545b73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb5ae22f46c005ddcb35",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "33074c83156ab3910437",
+    "url": "/static/css/main.25cd8d4d.chunk.css"
+  },
+  {
+    "revision": "cb5ae22f46c005ddcb35",
+    "url": "/static/js/2.99829072.chunk.js"
+  },
+  {
+    "revision": "857d129e4a5f4f33c7a4a143bf79109c",
+    "url": "/static/js/2.99829072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33074c83156ab3910437",
+    "url": "/static/js/main.f10641c0.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "4de8cbab7d15a47542670614d2749acd",
+    "url": "/static/media/c.4de8cbab.png"
+  },
+  {
+    "revision": "4de400e141dd21ede29245c359f886f9",
+    "url": "/static/media/mail.4de400e1.png"
+  },
+  {
+    "revision": "996f2d88972e2ad9d30aa73fc79315c9",
+    "url": "/static/media/portrait.996f2d88.JPG"
+  }
+]);
