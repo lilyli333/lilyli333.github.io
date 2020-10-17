3743af0803e066a4cc15b46e5f4b337f0f5852fb
Lily Li <lily.jiayu@gmail.com>
2020-10-17 12:23:42 -0500

    smooth animation skills

--- src/skills/turning_cards.js
+++ src/skills/turning_cards.js
@@ -19,28 +19,32 @@ export default function TurningCardsText(props) {
 }
 
 function Card(props) {
+    const variants = {
+        open: { opacity: 1, width: "20vw" },
+        closed: { opacity: 1, width: "30vw" },
+    }
     let [isOpen,setIsOpen]=React.useState(false);
     return(
       <div>
-        <motion.div
-        whileHover={{ scale: 1.2 }}
-        whileTap={{ scale: 0.8 }}
-            transition={{duration: 0.3}}
-        >
-        </motion.div>
         <div id="item-div">
+            <motion.div
+            animate={isOpen ? "closed" : "open"}
+            transition={{duration: 0.3}}
+            variants={variants}
+            >
             <div id="item-background">
                 <div className={isOpen ? "item-expanded" : "item-closed"} id="item">
                     <h6 hidden={isOpen}>{props.primary}</h6>
                     <h5 hidden={!isOpen}>{props.primary}</h5>
                     <p hidden={!isOpen} className="paragraph">{props.secondary}</p>
                 </div>
-                <Button onClick={() => setIsOpen(!isOpen)} id="card-button" style={{height:"fit-content"}}>
+                <Button onClick={() => setIsOpen(!isOpen)} id="card-button" style={{height:"fit-content"}} className="button">
                     <h1 hidden={isOpen}>+</h1>
                     <h1 hidden={!isOpen}>-</h1>
                 </Button>
             </div>
-            <div hidden={isOpen}>
+            </motion.div>
+            <div style={{position:"absolute", right:"5px", top: "50%", transform:"translate(0%, -50%)", width:"30%", padding:"7px"}}>
                 <h6>{props.description}</h6>
             </div>
         </div>
